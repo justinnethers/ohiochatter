657b60f097f3318e2c91044b510f26a7472714ff
Justin Nethers <justin.nethers@scripps.com>
2023-06-26 18:18:39 -0400

    debugging deployment

--- database/migrations/2023_06_23_024155_create_threads_table.php
+++ database/migrations/2023_06_23_024155_create_threads_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('threads', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+//        Schema::create('threads', function (Blueprint $table) {
+//            $table->id();
+//            $table->timestamps();
+//        });
     }
 
     /**
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('threads');
+//        Schema::dropIfExists('threads');
     }
 };
--- database/migrations/2023_06_24_190039_create_replies_table.php
+++ database/migrations/2023_06_24_190039_create_replies_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('replies', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+//        Schema::create('replies', function (Blueprint $table) {
+//            $table->id();
+//            $table->timestamps();
+//        });
     }
 
     /**
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('replies');
+//        Schema::dropIfExists('replies');
     }
 };

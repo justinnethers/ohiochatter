74a62e3ace7765e58292be209b4471742fb2b3cb
justin <justin.nethers@gmail.com>
2024-12-20 10:49:28 -0500

    Make sure unauthed folks can't see restricted forums.

--- app/Http/Controllers/ForumController.php
+++ app/Http/Controllers/ForumController.php
@@ -10,6 +10,10 @@ class ForumController extends Controller
 {
     public function show(Forum $forum)
     {
+        if (!auth()->check() && $forum->is_restricted) {
+            return redirect('login');
+        }
+
         $threads = Thread::where('forum_id', $forum->id)
             ->orderBy('updated_at', 'desc')
             ->paginate(config('forum.threads_per_page'));

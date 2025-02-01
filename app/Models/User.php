6585f779021ff1eeedbb1b11c50b72e7c5bd9a4d
justin <justin.nethers@gmail.com>
2025-01-31 23:18:15 -0500

    Cleanup refactor.

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,9 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use App\Models\Reply;
 use Cmgmyr\Messenger\Traits\Messagable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Carbon;
@@ -16,14 +15,8 @@ use Laravel\Scout\Searchable;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, Messagable;
-    use Searchable;
+    use HasApiTokens, HasFactory, Notifiable, Messagable, Searchable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'username',
         'email',
@@ -31,111 +24,103 @@ class User extends Authenticatable
         'name'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
 
+    public function replies(): HasMany
+    {
+        return $this->hasMany(Reply::class);
+    }
 
-    public function getAvatarPathAttribute($avatar)
+    public function searches(): HasMany
+    {
+        return $this->hasMany(Search::class);
+    }
+
+    public function getAvatarPathAttribute($avatar): string
     {
         if ($avatar) {
-            return 'https://ohiochatter.com/'.$avatar;
+            // Use a configurable base URL for flexibility.
+            return rtrim(config('app.avatar_base_url', config('app.url')), '/') . '/' . ltrim($avatar, '/');
         }
-
-//        return asset($avatar ?: 'images/avatars/default.png');
         return asset('images/avatars/default.png');
     }
 
-    public function hasRepliedTo(Thread $thread)
+    /**
+     * Determine if the user has replied to the given thread.
+     */
+    public function hasRepliedTo(Thread $thread): bool
     {
         return Cache::rememberForever(
             "user-{$this->id}-replied-to-thread-{$thread->id}",
             function () use ($thread) {
-                return (new Reply)->where('user_id', '=', $this->id)
-                    ->where('thread_id', '=', $thread->id)->count();
+                return $this->replies()->where('thread_id', $thread->id)->exists();
             }
         );
     }
 
-    // Mark when the user has read the given thread.
+    /**
+     * Mark when the user has read the given thread.
+     */
     public function read(Thread $thread): void
     {
         DB::table('threads_users_views')
-            ->where('user_id', $this->id)
-            ->where('thread_id', $thread->id)
             ->updateOrInsert(
                 ['user_id' => $this->id, 'thread_id' => $thread->id],
                 ['last_view' => Carbon::now()]
             );
     }
 
-    // Get the last time the user viewed the given thread.
-    public function lastViewedThreadAt($thread)
+    /**
+     * Retrieve the thread view record for the user.
+     */
+    public function threadViewRecord(Thread $thread): ?object
     {
-        $view = DB::table('threads_users_views')
+        return DB::table('threads_users_views')
             ->where('user_id', $this->id)
             ->where('thread_id', $thread->id)
             ->first();
-
-        return $view?->last_view;
-
     }
 
-    public function repliesPerPage()
+    /**
+     * Get the last time the user viewed the given thread.
+     */
+    public function lastViewedThreadAt(Thread $thread): ?Carbon
+    {
+        $record = $this->threadViewRecord($thread);
+        return $record ? new Carbon($record->last_view) : null;
+    }
+
+    /**
+     * Get the number of replies per page from configuration.
+     */
+    public function repliesPerPage(): int
     {
         return config('forum.replies_per_page');
     }
 
-    public function touchActivity()
+    /**
+     * Update the user's last activity timestamp.
+     */
+    public function touchActivity(): void
     {
         $this->last_activity = $this->freshTimestamp();
-        return $this->save();
+        $this->save();
     }
 
-    public function lastVisitToThread(Thread $thread)
-    {
-//        return Cache::remember("user-" . auth()->id() . "-viewed-thread-{$thread->id}", 7200, function() use ($thread) {
-            return DB::table('threads_users_views')
-                ->where('user_id', '=', auth()->user()->id)
-                ->where('thread_id', '=', $thread->id)
-                ->first();
-//        });
-    }
-
-//    public function getScoutKey(): mixed
-//    {
-//        return $this->username;
-//    }
-//
-//    public function getScoutKeyName(): mixed
-//    {
-//        return 'username';
-//    }
-
-    public function toSearchableArray()
+    /**
+     * Get the array representation of the model for Laravel Scout indexing.
+     */
+    public function toSearchableArray(): array
     {
         return [
             'username' => $this->username,
         ];
-    }
-
-    public function searches()
-    {
-        return $this->hasMany(Search::class);
     }
 }

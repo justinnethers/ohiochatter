b110eb3ad9ddb2b7a4abbb92c029e7e0540b3183
justin <justin.nethers@gmail.com>
2025-02-01 00:09:22 -0500

    Tighten up styling.

--- resources/views/components/thread/listing.blade.php
+++ resources/views/components/thread/listing.blade.php
@@ -16,34 +16,32 @@
         @endif
     </a>
 
-    <div class="md:flex text-lg justify-between rounded md:rounded-md px-2 mt-3 mb-4 bg-gray-800 shadow">
+    <div class="md:flex text-base justify-between rounded md:rounded-md px-2 mt-3 mb-4 bg-gray-800 shadow divide-y divide-gray-700">
 
-        <div class="flex items-center space-x-2">
+        <div class="flex items-center space-x-2 py-1.5 px-0.5">
             @if ($thread->owner)
-                <x-avatar size="8" :avatar-path="$thread->owner->avatar_path" />
+                <x-avatar size="6" :avatar-path="$thread->owner->avatar_path" />
                 <span>
                     started {{ \Carbon\Carbon::parse($thread->created_at)->setTimezone((auth()->check() ? auth()->user()->timezone : null))->diffForHumans() }}
-                    by <br class="md:hidden"><a href="/profiles/{{ $thread->owner->username }}" class="text-blue-500 hover:underline">{{ $thread->owner->username }}</a>
+                    by <a href="/profiles/{{ $thread->owner->username }}" class="text-blue-500 hover:underline">{{ $thread->owner->username }}</a>
                 </span>
             @endif
         </div>
 
-        <hr class="border-gray-700 border-2 md:hidden mt-2 mb-1.5">
-
-        <div class="flex items-center justify-end space-x-2 bg-main-color posted-by-when rounded shadow md:shadow-none md:p-2 md:p-0 md:m-0">
+        <div class="flex items-center justify-end space-x-2 bg-main-color posted-by-when rounded shadow md:shadow-none py-1.5 md:p-2 md:p-0 md:m-0">
             @if ($thread->lastReply)
                 <div class="text-right">
-                    <span class="md:mr-1">last post was</span>
+                    <span class="md:mr-1">last post </span>
                     <span class="md:mr-1">
                         {{ \Carbon\Carbon::parse($thread->lastReply->created_at)->setTimezone((auth()->check() ? auth()->user()->timezone : null))->diffForHumans() }}
-                        by<br class="md:hidden">
+                        by
                     </span>
                     <a href="/profiles/{{ $thread->lastReply->owner->username }}" class="text-blue-500 hover:underline">{{ $thread->lastReply->owner->username }}</a>
                 </div>
-                <x-avatar size="8" :avatar-path="$thread->lastReply->owner->avatar_path" />
+                <x-avatar size="6" :avatar-path="$thread->lastReply->owner->avatar_path" />
             @else
                 <div>
-                    <span class="md:mr-1">last post was</span>
+                    <span class="md:mr-1">last post </span>
                     <a href="/profiles/{{ $thread->owner->username }}">{{ $thread->owner->username }}</a>
                     <span>{{ \Carbon\Carbon::parse($thread->created_at)->setTimezone((auth()->check() ? auth()->user()->timezone : null))->diffForHumans() }}</span>
                 </div>

929eefdcec36bd4461cdc90267301ef5c2299ba2
justin <justin.nethers@gmail.com>
2025-02-03 19:11:02 -0500

    Fix text color in inputs.

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -2,4 +2,4 @@
 
 <input
     {{ $disabled ? 'disabled' : '' }}
-    {!! $attributes->merge(['class' => 'border-gray-700 dark:border-gray-700 dark:bg-gray-300 dark:text-gray-300 focus:border-indigo-600 dark:focus:border-indigo-600 focus:ring-indigo-600 dark:focus:ring-indigo-600 rounded-md shadow-sm p-1.5 px-3 text-lg w-full font-medium']) !!}>
+    {!! $attributes->merge(['class' => 'border-gray-700 dark:border-gray-700 dark:bg-gray-300 dark:text-gray-950 focus:border-indigo-600 dark:focus:border-indigo-600 focus:ring-indigo-600 dark:focus:ring-indigo-600 rounded-md shadow-sm p-1.5 px-3 text-lg w-full font-medium']) !!}>

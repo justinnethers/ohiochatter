fc03251c2b5d4b06555b862595c7a28fe2478126
justin <justin.nethers@gmail.com>
2025-02-16 13:29:39 -0500

    Move jquery plugin to page where jquery is actually loaded.

--- resources/views/components/wysiwyg.blade.php
+++ resources/views/components/wysiwyg.blade.php
@@ -117,5 +117,8 @@
                 }
             });
         </script>
+        <script>
+            !function(e){"use strict";var t={enabled:!0,endpoint:"https://noembed.com/embed?nowrap=on"};e.extend(!0,e.trumbowyg,{plugins:{pasteEmbed:{init:function(n){n.o.plugins.pasteEmbed=e.extend(!0,{},t,n.o.plugins.pasteEmbed||{}),Array.isArray(n.o.plugins.pasteEmbed.endpoints)&&(n.o.plugins.pasteEmbed.endpoint=n.o.plugins.pasteEmbed.endpoints[0]),n.o.plugins.pasteEmbed.enabled&&n.pasteHandlers.push((function(t){try{var a=(t.originalEvent||t).clipboardData.getData("Text");if(!a.startsWith("http"))return;a = a.replace(/https?:\/\/(www\.)?x\.com/g, 'https://twitter.com');var s=n.o.plugins.pasteEmbed.endpoint;t.stopPropagation(),t.preventDefault();var i=new URL(s);i.searchParams.append("url",a.trim()), fetch(i,{method:"GET",cache:"no-cache",signal:AbortSignal.timeout(2e3)}).then((e=>e.json().then((e=>e.html)))).catch((()=>{})).then((t=>{void 0===t&&(t=e("<a>",{href:a,text:a})[0].outerHTML), n.execCmd("insertHTML",t)}))}catch(e){}}))}}}})}(jQuery);
+        </script>
     </x-slot>
 </div>

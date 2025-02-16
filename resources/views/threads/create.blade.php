c6bb7cc0588b7494a2fcf3550f95af87c7643749
justin <justin.nethers@gmail.com>
2025-02-16 11:16:53 -0500

    Fix some styling.

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -8,7 +8,7 @@
 
     <div class="p-2">
         <form
-            class="p-2 md:p-8 rounded-lg shadow mb-8 md:mt-8"
+            class="px-0 md:px-8"
             action="/threads"
             method="POST"
         >

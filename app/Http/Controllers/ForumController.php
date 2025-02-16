10f5773e821942e9f034171e1c7ee05fb1a57f7d
justin <justin.nethers@gmail.com>
2025-02-15 20:34:19 -0500

    Turn off caching on forums index. Something is broken in the pagination.

--- app/Http/Controllers/ForumController.php
+++ app/Http/Controllers/ForumController.php
@@ -14,14 +14,14 @@ class ForumController extends Controller
             return redirect('login');
         }
 
-        $page = request()->get('page', 1);
-        $threads = Cache::remember("forum_{$forum->id}_threads_page_{$page}", now()->addDay(), function() use ($forum) {
-            return Thread::query()
+//        $page = request()->get('page', 1);
+//        $threads = Cache::remember("forum_{$forum->id}_threads_page_{$page}", now()->addDay(), function() use ($forum) {
+            $threads = Thread::query()
                 ->with(['owner', 'forum', 'poll'])
                 ->where('forum_id', $forum->id)
                 ->orderBy('last_activity_at', 'desc')
                 ->paginate(config('forum.threads_per_page'));
-        });
+//        });
 
         return view('forums.show', compact('forum', 'threads'));
     }

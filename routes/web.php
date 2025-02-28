5f36ff0ec8dc79808739526032c84d1ba1bc8f14
justin <justin.nethers@gmail.com>
2025-02-27 20:31:39 -0500

    Updated routes.

--- routes/web.php
+++ routes/web.php
@@ -186,13 +186,15 @@ Route::get('/buckEYE', [BuckEyeGameController::class, 'index'])
     ->name('buckeye.index');
 
 // Guest play route (no authentication required)
-Route::get('/buckeye/play', [BuckEyeGameController::class, 'guestPlay'])
+Route::get('/buckEYE/play', [BuckEyeGameController::class, 'guestPlay'])
     ->name('buckeye.guest');
 
 // Stats page (authenticated users only)
 Route::middleware(['auth'])->group(function () {
-    Route::get('/buckeye/stats', [BuckEyeGameController::class, 'stats'])
+    Route::get('/buckEYE/stats', [BuckEyeGameController::class, 'stats'])
         ->name('buckeye.stats');
 });
 
-Route::redirect('buckeye', 'buckEYE');
+Route::redirect('buckeye', '/buckEYE');
+Route::redirect('buckeye/stats', '/buckEYE/stats');
+Route::redirect('buckeye/play', '/buckEYE/play');

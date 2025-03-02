b837579df08de6fbf4ae36dbc3a9c5ce6e704149
justin <justin.nethers@gmail.com>
2025-03-02 08:03:53 -1000

    Prettier stuff.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,19 +1,19 @@
-import './bootstrap';
+import './bootstrap'
 
 import Alpine from 'alpinejs'
 window.Alpine = Alpine
 Alpine.start()
 
 window.addEventListener('scroll', () => {
-    const nav = document.querySelector('nav');
-    const header = document.querySelector('header.bg-gray-800');
-    const scrolled = window.pageYOffset > 60;
+  const nav = document.querySelector('nav')
+  const header = document.querySelector('header.bg-gray-800')
+  const scrolled = window.pageYOffset > 60
 
-    nav.classList.toggle('h-12', scrolled);
-    nav.classList.toggle('h-16', !scrolled);
+  nav.classList.toggle('h-12', scrolled)
+  nav.classList.toggle('h-16', !scrolled)
 
-    if (header) {
-        header.classList.toggle('py-3', scrolled);
-        header.classList.toggle('py-6', !scrolled);
-    }
-});
+  if (header) {
+    header.classList.toggle('py-3', scrolled)
+    header.classList.toggle('py-6', !scrolled)
+  }
+})

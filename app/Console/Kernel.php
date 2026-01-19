a47191907ce430b36d394e29f966b715daaff28a
justin <justin.nethers@gmail.com>
2026-01-18 20:04:40 -0500

    Fix import.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\ProcessThreadsForSeo;
 use App\Modules\OhioWordle\Commands\CreateDailyPuzzle;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
